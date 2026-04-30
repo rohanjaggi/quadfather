058263004d465eb9e082e63519b40d402dcaffd0
rohanjaggi <jaggirohan@yahoo.com>
2026-04-30 22:35:54 +0800

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <BottomNav />
           </UserProvider>
         </TelegramProvider>
+        <Analytics />
       </body>
     </html>
   )
